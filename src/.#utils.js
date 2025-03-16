a7e112bcaeebbe72e8e4419b6a6eff53d5fbf499
welldirn <wdirnbeck@gmail.com>
2025-03-16 16:47:58 -0500

    SJF complete, added time mechanism to step each process every second. Simulation settings WIP

---
+++ src/.#utils.js
@@ -0,0 +1,1 @@
+will@Wills-Air.attlocal.net.31135
--- src/App.js
+++ src/App.js
@@ -1,32 +1,62 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./styles.css";
-import {createProc, process, ProcListDeets, FIFOChart, Context, StepFIFO} from "./utils.js";
+import {createProc, process, ProcListDeets, FIFOChart, SJFChart, Context, StepFIFO, StepSJF} from "./utils.js";
 
 
 function App() {
 
+    //User generated proclist
     const[procList, setProcList] = useState([]);
 
+    //FIFO stuff
     const[FIFO_procList, setFIFOProcList] = useState([]);
     const[FIFO_context, setFIFOContext] = useState(new Context());
     const[FIFO_data, setFIFOdata] = useState([]);
-    
-    const[instrLen, setProcLen] = useState(0);
-   
-   
+
+    //SJF stuff
+    const[SJF_procList, setSJFProcList] = useState([]);
+    const[SJF_context, setSJFContext] = useState(new Context());
+    const[SJF_data, setSJFdata] = useState([]);
+
+    //STCF stuff
+
+    //MLFQ stuff
+
+    //Simulation display setting
+    const[displayOption setDisplayOption] = useState("");
+
+    //Time quantum counter and start status
+    const[timeQuant , setTimeQuant] = useState(0);
+    let[running, setRunning] = useState(false);
+
+    //CPU scheduler clock 1hz
+    useEffect(() => {
+	
+	const interval = setInterval(() => running &&  setTimeQuant(timeQuant + 1), 1000);
+
+	handleStep();
+
+	
+	return () => clearInterval(interval);
+	
+    },[timeQuant, running]);
+
+    function handleDisplay(event){
+	setDisplayOption(event.target.value);
+    }
+
 
     function handleProcSubmit(event){
 	
 	event.preventDefault();
 	const formData = new FormData(event.target)
         const numProc = Number(formData.get("quantity"));
-
-	
 	const newProcList = createProc(numProc); //resturns process list of size numProc
 	setProcList(newProcList); //updates state of procList with newProcList (just created)
 
+	
+	//FIFO//
 	//proclists for every operation need to be created using structuredClone of newProclist
-	setFIFOProcList( structuredClone(newProcList) );
 	setFIFOProcList(prevState => {
 	    const newList = structuredClone(newProcList);
 
@@ -47,41 +77,82 @@ function App() {
 	    return newList; // Ensures state is updated properly
 	});
 
+	//SJF//
+	setSJFProcList(prevState => {
+	    const newList = structuredClone(newProcList);
 
+	    let labels = [];
+	    let values = [];
+
+	    for (let i = 0; i < newList.length; i++) {
+		let pc = SJF_context.getPC(newList[i]);
+		let progress = newList[i].instrArr.length - pc;
+
+		labels.push(newList[i].id);
+		values.push(progress);
+	    }
+
+	    console.log("Updated SJF Data:", values);
+	    setSJFdata([values, labels]);
+
+	    return newList; // Ensures state is updated properly
+	});
+	
 
     }
     
 
-    function handleStep(event){
-
-	//step FIFO
-
-	let {wProcList , wContext} = StepFIFO(FIFO_procList, FIFO_context);
-	setFIFOProcList(wProcList);
-	setFIFOContext(wContext);
-
-	//Organize data to send to chart (FIFOdata)
-
+    function handleStep(){
 	let labels = [];
 	let values = [];
+	let pc = 0;
+	let progress = 0;
+	
+	
+
+	//FIFO//
+	let {fProcList , fContext} = StepFIFO(FIFO_procList, FIFO_context);
+	setFIFOProcList(fProcList);
+	setFIFOContext(fContext);
+
+	//Organize data to send to chart (FIFOdata)
+	labels = [];
+	values = [];
 
 	for(let i = 0; i < FIFO_procList.length ; i++){
 
-	    let pc = FIFO_context.getPC(FIFO_procList[i]);
-	    let progress = (FIFO_procList[i].instrArr.length - pc);
+	    pc = FIFO_context.getPC(FIFO_procList[i]);
+	    progress = (FIFO_procList[i].instrArr.length - pc);
 
-	    //check setter in context , id setting may not be working
+	    //TODO: check setter in context , id setting may not be working
 	    labels.push(FIFO_procList[i].id);
 	    values.push(progress);
-
-	    console.log(labels);
 	    
 	}
-
 	setFIFOdata([values, labels]);
 	
+
+	//SJF//
+	let {sProcList, sContext} = StepSJF(SJF_procList, SJF_context);
+	setSJFProcList(sProcList);
+	setSJFContext(sContext);
+
+	labels = [];
+	values = [];
+
+	for(let i = 0; i < SJF_procList.length ; i++){
+
+	    pc = SJF_context.getPC(SJF_procList[i]);
+	    progress = (SJF_procList[i].instrArr.length - pc);
+
+	    labels.push(SJF_procList[i].id);
+	    values.push(progress);
+	    
+	}
+	setSJFdata([values, labels]);
+	
     }
-    
+
     return (
   <>
         <div className="center">
@@ -95,22 +166,55 @@ function App() {
 	    <button type="submit">Create Processes</button>
 	</form>
 
-      <div>
+      <div className="top-header-group">
+	  
+	  <div className="lr-header-sections">
 
-	  <ProcListDeets procs={procList}/>
+	    <h2>Generated Process Properties</h2>
+	      <ul>
+		  {procList.map((proc, index) => (
+		      <li key={index}>
+			  Process: {index} -  Number of instructions: {proc.getProcLen()}
+		      </li>
+		  ))}
+	      </ul>
+	    
+	  </div>
+	  
+	  <div className="lr-header-sections">
+	      <h2>Simulation Settings</h2>
+	      <label> htmlFor="options">Choose Simulation to Display</label>
+
+	      
+	      <button onClick={() => setRunning((prevRunning) => !prevRunning)}>Start/Stop</button>
+
+
+	     
+	 </div>
 	  
       </div>
 
+      <hr />
 
-      <div style={{ width: "600px", margin: "auto" }}>
-	  <h2>FIFO</h2>
-	  <FIFOChart indata={FIFO_data} />
-	  
-	  <button onClick = {handleStep}>Step once</button>
+      <div className="chart-grid">
+	   
+	  <div className="chart-box">
+	      <h2>FIFO</h2>
+	      <FIFOChart indata={FIFO_data} />
+	      <button onClick = {handleStep}>Step once</button>
 
+	  </div>
 	  
+	  <div className="chart-box">
+
+	      <h2>SJF</h2>
+	      <SJFChart indata={SJF_data} />
+	      <button onClick = {handleStep}>Step once</button>
+	  
+	  </div>
+
       </div>
-
+      
  </>   
   
     );
